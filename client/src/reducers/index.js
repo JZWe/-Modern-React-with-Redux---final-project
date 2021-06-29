85486450037f3af9571b9dd9a7fda8ca2c9d8d63
JZWe <ArturiaTW@gmail.com>
2021-06-29 10:16:30 +0800

    Delete client directory

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-import authReducer from './authReducer';
-
-export default combineReducers({
-  auth: authReducer,
-  form: formReducer
-});
